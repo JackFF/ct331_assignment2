961010b3bed77be82cb8c6f1a9951ad767eee5d7
Jack Foley <j.foley11@nuigalway.ie>
2017-10-26 20:39:06 +0100

    Assignment 2

--- assignment_q1.rkt
+++ assignment_q1.rkt
@@ -1,1 +1,11 @@
 #lang racket
+
+(cons 1 2)
+
+(cons 1 (cons 2 (cons 3 empty)))
+
+(cons "string" (cons 1 (cons '(2 3 4) empty)))
+
+(list 'string '1 '(2 3 4))
+
+(append '(string) '(1) '((2 3 4)))
--- assignment_q2.rkt
+++ assignment_q2.rkt
@@ -12,6 +12,39 @@
 ;You may delete these comments!
 
 (provide ins_beg)
+(provide ins_end)
+(provide cout_top_level)
+(provide count_instances)
+(provide count_instances_tr)
+(provide count_instances_deep)
 
 (define (ins_beg el lst)
-  (display "Hello, I'm ins_beg!\n"))
+  (cons el (cons lst empty)))
+
+(define (ins_end el lst)
+  (cons lst (cons el empty)))
+
+(define (cout_top_level lst)
+  (if(null? lst)
+     0
+     (+ 1 (cout_top_level(cdr lst)))))
+
+(define (count_instances lst item)
+  (cond
+    [(empty? lst) 0]
+    [(equal? item (car lst))
+        (+ 1 (count_instances (cdr lst) item))]
+    [else (count_instances (cdr lst) item)]
+  )
+ )
+
+(define (count_instances_tr item lst)
+  (tr_helper item lst 0)
+  )
+
+(define (tr_helper item lst total)
+  (cond [(empty? lst) total]
+        [(equal? item (car lst)) (tr_helper item (cdr lst) (+ 1 total))]
+        [else (tr_helper item (cdr lst) total)]
+   )
+)
